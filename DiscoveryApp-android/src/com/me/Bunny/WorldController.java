bdde42fcdf9afac0400939104d5f2d6ecd9339b9
quyenha2511 <quyenha2511@eagles.ewu.edu>
2014-07-09 17:05:17 -0700

    saving for testing

--- DiscoveryApp-android/src/com/me/Bunny/WorldController.java
+++ DiscoveryApp-android/src/com/me/Bunny/WorldController.java
@@ -13,30 +13,124 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.input.GestureDetector;
 import com.badlogic.gdx.input.GestureDetector.GestureListener;
 import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 
-public class WorldController implements GestureListener{
+import drawingHandler.Coins;
+import drawingHandler.Dinasour;
+import drawingHandler.Dinasour.JUMP_STATE;
+import drawingHandler.Rocks;
+import drawingHandler.SpecialCoin;
+
+public class WorldController implements GestureListener {
 	private static final String TAG = WorldController.class.getName();
 
 	public LevelLoader level;
 	public CameraHelper cameraHelper;
 	public int lives;
 	public int score;
-	
+
+	private Rectangle r1 = new Rectangle();
+	private Rectangle r2 = new Rectangle();
+
 	public WorldController() {
 		init();
 	}
 
 	public void init() {
-		//initTestObjects();
+		// initTestObjects();
 		initLevel();
 		cameraHelper = new CameraHelper();
 		Gdx.input.setInputProcessor(new GestureDetector(this));
 		lives = Constants.LIVES_START;
-		
+
 	}
-	
-	public int getLives(){
+
+	private void onCollisionHeadWithRock(Rocks rock) {
+		Dinasour dinasour = level.dinasour;
+		float heightDifference = Math.abs(dinasour.position.y
+				- (rock.position.y + rock.bounds.height));
+		if (heightDifference > 0.25f) {
+			boolean hitLeftEdge = dinasour.position.x > (rock.position.x + rock.bounds.width / 2.0f);
+			if (hitLeftEdge) {
+				dinasour.position.x = rock.position.x + rock.bounds.width;
+			} else {
+				dinasour.position.x = rock.position.x - dinasour.bounds.width;
+			}
+			return;
+		}
+		switch (dinasour.jumpState) {
+		case GROUNDED:
+			break;
+		case FALLING:
+		case JUMP_FALLING:
+			dinasour.position.y = rock.position.y + dinasour.bounds.height
+					+ dinasour.origin.y;
+			dinasour.jumpState = JUMP_STATE.GROUNDED;
+			break;
+		case JUMP_RISING:
+			dinasour.position.y = rock.position.y + dinasour.bounds.height
+					+ dinasour.origin.y;
+			break;
+		}
+	}
+
+	private void onCollisionWithCoin(Coins coin) {
+		coin.collected = true;
+		score += coin.getScore();
+		Gdx.app.log(TAG, "Gold coin collected");
+	}
+
+	private void onCollisionWithSpecialCoin(SpecialCoin specialcoin) {
+		specialcoin.collected = true;
+		score += specialcoin.getScore();
+		level.dinasour.setSpecialCoinPowerUp(true);
+		Gdx.app.log(TAG, "Feather collected");
+	}
+
+	private void testCollisions() {
+		r1.set(level.dinasour.position.x, level.dinasour.position.y,
+				level.dinasour.bounds.width, level.dinasour.bounds.height);
+		// Test collision: Bunny Head <-> Rocks
+		for (Rocks rock : level.rocks) {
+			r2.set(rock.position.x, rock.position.y, rock.bounds.width,
+					rock.bounds.height);
+			if (!r1.overlaps(r2))
+				continue;
+			onCollisionHeadWithRock(rock);
+			// IMPORTANT: must do all collisions for valid
+			// edge testing on rocks.
+		}
+		// Test collision: Bunny Head <-> Gold Coins
+		for (Coins goldcoin : level.coins) {
+			if (goldcoin.collected)
+				continue;
+			r2.set(goldcoin.position.x, goldcoin.position.y,
+					goldcoin.bounds.width, goldcoin.bounds.height);
+			if (!r1.overlaps(r2))
+				continue;
+			onCollisionWithCoin(goldcoin);
+			break;
+		}
+		// Test collision: Bunny Head <-> Feathers
+		for (SpecialCoin feather : level.specialCoin) {
+			if (feather.collected)
+				continue;
+			r2.set(feather.position.x, feather.position.y,
+					feather.bounds.width, feather.bounds.height);
+			if (!r1.overlaps(r2))
+				continue;
+			onCollisionBunnyWithFeather(feather);
+			break;
+		}
+	}
+
+	private void onCollisionBunnyWithFeather(SpecialCoin feather) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public int getLives() {
 		return lives;
 	}
 
@@ -63,6 +157,7 @@ public class WorldController implements GestureListener{
 
 	public void update(float deltaTime) {
 		level.update(deltaTime);
+		testCollisions();
 		cameraHelper.update(deltaTime);
 	}
 
@@ -74,22 +169,24 @@ public class WorldController implements GestureListener{
 
 	@Override
 	public boolean tap(float x, float y, int count, int button) {
-		//Log.d(TAG, "current  at : "+ testSprites[selectedSprite].getX() +  " , "+ testSprites[selectedSprite].getY());
-		//moveSelectedSprite(  0  , 5* Gdx.graphics.getDeltaTime() );
-		//Log.d(TAG, "tap at : "+ x +  " , "+ y);
-		
-		/*selectedSprite = (selectedSprite+1)% testSprites.length;
-		if(cameraHelper.hasTarget()){
-			cameraHelper.setTarget(testSprites[selectedSprite]);
-		}
-		cameraHelper.setTarget(cameraHelper.hasTarget() ? null :
-			testSprites[selectedSprite]);*/
+		// Log.d(TAG, "current  at : "+ testSprites[selectedSprite].getX() +
+		// " , "+ testSprites[selectedSprite].getY());
+		// moveSelectedSprite( 0 , 5* Gdx.graphics.getDeltaTime() );
+		// Log.d(TAG, "tap at : "+ x + " , "+ y);
+
+		/*
+		 * selectedSprite = (selectedSprite+1)% testSprites.length;
+		 * if(cameraHelper.hasTarget()){
+		 * cameraHelper.setTarget(testSprites[selectedSprite]); }
+		 * cameraHelper.setTarget(cameraHelper.hasTarget() ? null :
+		 * testSprites[selectedSprite]);
+		 */
 		return false;
 	}
 
 	@Override
 	public boolean longPress(float x, float y) {
-		
+
 		return false;
 	}
 
@@ -107,7 +204,7 @@ public class WorldController implements GestureListener{
 
 	@Override
 	public boolean zoom(float initialDistance, float distance) {
-		//moveSelectedSprite(  0  , -5* Gdx.graphics.getDeltaTime() );
+		// moveSelectedSprite( 0 , -5* Gdx.graphics.getDeltaTime() );
 		return false;
 	}
 
